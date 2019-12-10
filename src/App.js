f4166a59cb39744b2798a77df874f3d5fe1bb405
Juan D. Jara <juanorigami@gmail.com>
2019-12-10 20:21:48 +0100

    loading styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
 //
 import { Router } from '@reach/router'
+import Loading from './components/Loading'
 
 function App() {
   return (
     <Root>
       <div className="content">
-        <React.Suspense fallback={<p style={{ padding: '2rem', textAlign: 'center' }}>Loading...</p>}>
+        <React.Suspense fallback={<Loading />}>
           <Router>
             <Routes path="*" />
           </Router>
---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loading = () => (
+  <div style={{
+    fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center'
+  }}>
+    <p style={{marginBottom: 8, fontSize: '1.4rem'}}>
+      <strong>Asociación Guardianes</strong>
+    </p>
+    <span>Cargando ...</span>
+  </div>
+)
+
+export default Loading
