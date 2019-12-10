e41247e980ffcebcc71b5d16fba5e9919ecbbd80
Juan D. Jara <juanorigami@gmail.com>
2019-12-10 23:56:51 +0100

    separating components

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -16,33 +16,126 @@ function groupPosts (posts) {
   return Object.values(reduced)
 }
 
+function PostsHeader ({ sections, currentSection }) {
+  return (
+    <header>
+      <nav>
+        <Link className="nav-root" to="/">
+          <img className="site-logo" src="/images/escudo-flat-blanco.png" alt="logo" />
+          <h1 className="title">Asociación Guardianes</h1>
+        </Link>
+        <ul>
+          {sections.map(s => (
+            <li key={s.id} className={s.id === currentSection.id ? 'selected' : ''}>
+              <Link to={`/${s.id}`}>
+                <img src={s.icono && `${api.thumbsUrl}/200/200/crop/good/${s.icono.filename}`} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <section className="container section-description">
+        <h1>{currentSection.titulo}</h1>
+        <div className="html-content" dangerouslySetInnerHTML={{ __html: currentSection.descripcion }}></div>
+      </section>
+      <style jsx>{`
+header {
+  color: white;
+  background: linear-gradient(120deg, #8b1f27, #c41525);
+  padding-bottom: 32px;
+}
+
+nav {
+  padding: 1rem;
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+:global(nav a) {
+  text-decoration: none;
+  color: white;
+}
+
+:global(nav a.nav-root) {
+  align-items: center;
+  margin-bottom: 24px;
+  display: flex;
+}
+
+nav ul {
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-end;
+}
+
+:global(nav ul a) {
+  display: block;
+}
+
+@media (max-width: ${CONTAINER_WIDTH}px) {
+  nav ul {
+    justify-content: center;
+  }
+}
+
+nav ul li {
+  padding-bottom: 8px;
+  border-bottom: 3px solid transparent;
+  margin: 0 8px;
+  text-align: center;
+}
+
+nav ul li p {
+  margin: 0;
+}
+
+nav ul img {
+  height: 45px;
+}
+
+nav ul li:hover,
+nav ul li.selected {
+  border-bottom-color: white;
+}
+
+:global(.nav-root h1) {
+  font-size: 2rem;
+  margin: 0;
+}
+
+.site-logo {
+  height: 60px;
+  margin-right: 1rem;
+}
+
+.container {
+  max-width: ${CONTAINER_WIDTH}px;
+  padding: 0 16px;
+  margin: 0 auto;
+}
+
+.section-description h1 {
+  font-size: 4rem;
+  margin: 16px 0;
+}
+
+.section-description .html-content {
+  font-size: 20px;
+}
+      `}</style>
+    </header>
+  )
+}
+
 function Posts () {
   const { sections = [], currentSection = {}, posts = [] } = useRouteData()
   const groupedPosts = groupPosts(posts)
+  console.log('grouped-posts', groupedPosts)
   return (
     <div className="page">
       <HeadConfig />
-      <header>
-        <nav>
-          <Link className="nav-root" to="/">
-            <img className="site-logo" src="/images/escudo-flat-blanco.png" alt="logo" />
-            <h1 className="title">Asociación Guardianes</h1>
-          </Link>
-          <ul>
-            {sections.map(s => (
-              <li key={s.id} className={s.id === currentSection.id ? 'selected' : ''}>
-                <Link to={`/${s.id}`}>
-                  <img src={s.icono && `${api.thumbsUrl}/200/200/crop/good/${s.icono.filename}`} />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <section className="container section-description">
-          <h1>{currentSection.titulo}</h1>
-          <div className="html-content" dangerouslySetInnerHTML={{ __html: currentSection.descripcion }}></div>
-        </section>
-      </header>
+      <PostsHeader sections={sections} currentSection={currentSection} />
       <div className={posts.length ? 'posts container card' : 'posts container'}>
         {groupedPosts.map(g => (
           <ul key={g.seccion}>
@@ -59,133 +152,62 @@ function Posts () {
       </div>
       <Footer></Footer>
       <style jsx>{`
-        .page {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          background-color: #f8f8f8;
-        }
+.page {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #f8f8f8;
+}
 
-        header {
-          color: white;
-          background: linear-gradient(120deg, #8b1f27, #c41525);
-          padding-bottom: 32px;
-        }
+.container {
+  max-width: ${CONTAINER_WIDTH}px;
+  padding: 0 16px;
+  margin: 0 auto;
+}
 
-        nav {
-          padding: 1rem;
-        }
-        :global(nav a) {
-          text-decoration: none;
-          color: white;
-        }
-        nav,
-        :global(nav .nav-root),
-        nav ul {
-          display: flex;
-        }
-        nav {
-          align-items: flex-start;
-          flex-wrap: wrap;
-        }
-        :global(.nav-root) {
-          align-items: center;
-          margin-bottom: 24px;
-        }
-        nav ul {
-          flex-grow: 1;
-          justify-content: flex-end;
-        }
-        :global(nav ul a) {
-          display: block;
-        }
+@media (min-width: ${CONTAINER_WIDTH}px) {
+  .card {
+    box-shadow: 2px 2px 6px 0px rgba(0,0,0,0.2);
+    border-radius: 8px;
+    background-color: white;
+  }
 
-        @media (max-width: 812px) {
-          nav ul {
-            justify-content: center;
-          }
-        }
+  .posts {
+    margin-top: -24px;
+  }
+}
 
-        nav ul li {
-          padding-bottom: 8px;
-          border-bottom: 3px solid transparent;
-          margin: 0 8px;
-          text-align: center;
-        }
-        nav ul li p {
-          margin: 0;
-        }
-        nav ul img {
-          height: 45px;
-        }
-        nav ul li:hover,
-        nav ul li.selected {
-          border-bottom-color: white;
-        }
+.posts {
+  flex-grow: 1;
+}
 
-        :global(.nav-root h1) {
-          font-size: 2rem;
-          margin: 0;
-        }
+.posts h2 {
+  font-size: 30px;
+}
 
-        .site-logo {
-          height: 60px;
-          margin-right: 1rem;
-        }
+.post {
+  flex: 1 1 50%;
+  min-width: 320px;
+  margin: 42px 0;
+}
 
-        .container {
-          max-width: ${CONTAINER_WIDTH}px;
-          padding: 0 16px;
-          margin: 0 auto;
-        }
+.post h3 {
+  font-size: 20px;
+}
 
-        .section-description h1 {
-          font-size: 4rem;
-          margin: 16px 0;
-        }
+.post .html-content {
+  line-height: 1.6;
+}
 
-        .section-description .html-content {
-          font-size: 20px;
-        }
+.tags span {
+  background-color: #eee;
+  padding: 4px 8px;
+  border-radius: 4px;
+}
 
-        .card {
-          box-shadow: 2px 2px 6px 0px rgba(0,0,0,0.2);
-          border-radius: 8px;
-          background-color: white;
-        }
-        
-        .posts {
-          flex-grow: 1;
-          margin-top: -24px;
-        }
-
-        .posts h2 {
-          font-size: 30px;
-        }
-
-        .post {
-          flex: 1 1 50%;
-          min-width: 320px;
-          margin: 42px 0;
-        }
-
-        .post h3 {
-          font-size: 20px;
-        }
-
-        .post .html-content {
-          line-height: 1.6;
-        }
-
-        .tags span {
-          background-color: #eee;
-          padding: 4px 8px;
-          border-radius: 4px;
-        }
-
-        .tags span + span {
-          margin-left: 8px;
-        }
+.tags span + span {
+  margin-left: 8px;
+}
       `}</style>
     </div>
   )
