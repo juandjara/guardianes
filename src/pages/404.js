36cda653b431a5ef41ded7c278d8d9ee3bcb9a53
Juan D. Jara <juanorigami@gmail.com>
2020-06-07 01:55:14 +0200

    move styles from other pages to styled components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,38 +1,37 @@
 import React from 'react'
 import { Link } from '@reach/router'
+import styled from 'styled-components'
 
-export default () => (
-  <div className="page">
-    <h1>404 - Aquí no hay nada</h1>
-    <Link className="back-link" to="/">Volver</Link>
-    <style jsx>{`
-      :global(html, body) {
-        margin: 0;
-        font-family: 'Alegreya Sans', -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
-      }
-      .page {
-        color: white;
-        background: linear-gradient(120deg, #8b1f27, #c41525);
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-      }
-      h1, p {
-        margin: 0;
-      }
-      :global(.back-link) {
-        color: white;
-        margin-top: 1rem;
-        font-size: 16px;
-        opacity: 0.8;
-        text-decoration: none;
-      }
-      :global(.back-link):hover {
-        text-decoration: underline;
-        opacity: 1;
-      }
-    `}</style>
-  </div>
-)
+const NotFoundStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  h1, p {
+    margin: 0;
+  }
+
+  .back-link {
+    color: white;
+    margin-top: 1rem;
+    font-size: 16px;
+    opacity: 0.8;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+      opacity: 1;
+    }
+  }
+`
+
+export default function NotFound () {
+  return (
+    <NotFoundStyle className="page not-found">
+      <h1>404 - Aquí no hay nada</h1>
+      <Link className="back-link" to="/">Volver</Link>
+    </NotFoundStyle>
+  )
+}
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 import api from '../../cmsapi'
 import { Link } from '@reach/router'
 import Footer from '../components/Footer'
-import { useRouteData, useSiteData } from 'react-static'
+import { useRouteData } from 'react-static'
+import styled from 'styled-components'
 
 const CONTAINER_WIDTH = 812;
 
@@ -15,9 +16,96 @@ function groupPosts (posts) {
   return Object.values(reduced)
 }
 
+const PostHeaderStyles = styled.header`
+  header {
+    color: white;
+    background: linear-gradient(120deg, #8b1f27, #c41525);
+    padding-bottom: 32px;
+  }
+
+  nav {
+    padding: 1rem;
+    display: flex;
+    align-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: white;
+  }
+
+  nav a.nav-root {
+    align-items: center;
+    margin-bottom: 24px;
+    display: flex;
+  }
+
+  nav ul {
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+  }
+
+  nav ul a {
+    display: block;
+  }
+
+  @media (max-width: ${CONTAINER_WIDTH}px) {
+    nav ul {
+      justify-content: center;
+    }
+  }
+
+  nav ul li {
+    padding-bottom: 8px;
+    border-bottom: 3px solid transparent;
+    margin: 0 8px;
+    text-align: center;
+  }
+
+  nav ul li p {
+    margin: 0;
+  }
+
+  nav ul img {
+    height: 45px;
+  }
+
+  nav ul li:hover,
+  nav ul li.selected {
+    border-bottom-color: white;
+  }
+
+  .nav-root h1 {
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  .site-logo {
+    height: 60px;
+    margin-right: 1rem;
+  }
+
+  .container {
+    max-width: ${CONTAINER_WIDTH}px;
+    padding: 0 16px;
+    margin: 0 auto;
+  }
+
+  .section-description h1 {
+    font-size: 4rem;
+    margin: 16px 0;
+  }
+
+  .section-description .item-html {
+    font-size: 20px;
+  }
+`
+
 function PostsHeader ({ sections, currentSection }) {
   return (
-    <header>
+    <PostHeaderStyles className="post-header">
       <nav>
         <Link className="nav-root" to="/">
           <img className="site-logo" src="/images/escudo-flat-blanco.png" alt="logo" />
@@ -37,99 +125,43 @@ function PostsHeader ({ sections, currentSection }) {
         <h1>{currentSection.titulo}</h1>
         <div className="item-html" dangerouslySetInnerHTML={{ __html: currentSection.descripcion }}></div>
       </section>
-      <style jsx>{`
-        header {
-          color: white;
-          background: linear-gradient(120deg, #8b1f27, #c41525);
-          padding-bottom: 32px;
-        }
-
-        nav {
-          padding: 1rem;
-          display: flex;
-          align-items: flex-start;
-          flex-wrap: wrap;
-        }
-
-        :global(nav a) {
-          text-decoration: none;
-          color: white;
-        }
-
-        :global(nav a.nav-root) {
-          align-items: center;
-          margin-bottom: 24px;
-          display: flex;
-        }
-
-        nav ul {
-          display: flex;
-          flex-grow: 1;
-          justify-content: flex-end;
-        }
-
-        :global(nav ul a) {
-          display: block;
-        }
-
-        @media (max-width: ${CONTAINER_WIDTH}px) {
-          nav ul {
-            justify-content: center;
-          }
-        }
-
-        nav ul li {
-          padding-bottom: 8px;
-          border-bottom: 3px solid transparent;
-          margin: 0 8px;
-          text-align: center;
-        }
-
-        nav ul li p {
-          margin: 0;
-        }
-
-        nav ul img {
-          height: 45px;
-        }
-
-        nav ul li:hover,
-        nav ul li.selected {
-          border-bottom-color: white;
-        }
-
-        :global(.nav-root h1) {
-          font-size: 2rem;
-          margin: 0;
-        }
-
-        .site-logo {
-          height: 60px;
-          margin-right: 1rem;
-        }
-
-        .container {
-          max-width: ${CONTAINER_WIDTH}px;
-          padding: 0 16px;
-          margin: 0 auto;
-        }
-
-        .section-description h1 {
-          font-size: 4rem;
-          margin: 16px 0;
-        }
-
-        .section-description .item-html {
-          font-size: 20px;
-        }
-      `}</style>
-    </header>
+    </PostHeaderStyles>
   )
 }
 
+const FixedMenuStyles = styled.section`
+  .fixed-menu {
+    position: absolute;
+    top: 24px;
+    left: 100%;
+    width: 100%;
+    max-width: 240px;
+    padding: 0 16px;
+  }
+  @media (max-width: 1140px) {
+    .fixed-menu {
+      display: none;
+    }
+  }
+  .fixed-menu h2 {
+    margin-bottom: 12px;
+    margin-top: 2rem;
+  }
+  .fixed-menu li {
+    margin-bottom: 8px;
+  }
+  .fixed-menu a {
+    text-decoration: none;
+    color: inherit;
+  }
+  .fixed-menu a:hover {
+    text-decoration: underline;
+  }
+`
+
 function FixedMenu ({ groupedPosts }) {
   return (
-    <section className="fixed-menu">
+    <FixedMenuStyles className="fixed-menu">
       {groupedPosts.map(g => (
         <ul key={g.seccion}>
           <h2>{g.seccion}</h2>
@@ -140,38 +172,102 @@ function FixedMenu ({ groupedPosts }) {
           ))}
         </ul>
       ))}
-      <style jsx>{`
-        .fixed-menu {
-          position: absolute;
-          top: 24px;
-          left: 100%;
-          width: 100%;
-          max-width: 240px;
-          padding: 0 16px;
-        }
-        @media (max-width: 1140px) {
-          .fixed-menu {
-            display: none;
-          }
-        }
-        .fixed-menu h2 {
-          margin-bottom: 12px;
-          margin-top: 2rem;
-        }
-        .fixed-menu li {
-          margin-bottom: 8px;
-        }
-        .fixed-menu a {
-          text-decoration: none;
-          color: inherit;
-        }
-        .fixed-menu a:hover {
-          text-decoration: underline;
-        }
-      `}</style>
-    </section>
+    </FixedMenuStyles>
   )
 }
+
+const PostStyles = styled.div`
+  .page {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: #f8f8f8;
+  }
+
+  .container {
+    max-width: ${CONTAINER_WIDTH}px;
+    padding: 0 16px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: ${CONTAINER_WIDTH}px) {
+    .card {
+      box-shadow: 2px 2px 6px 0px rgba(0,0,0,0.2);
+      border-radius: 8px;
+      background-color: white;
+    }
+
+    .posts {
+      margin-top: -24px;
+    }
+  }
+
+  .posts {
+    flex-grow: 1;
+    position: relative;
+  }
+
+  .posts h2 {
+    font-size: 30px;
+  }
+
+  .post {
+    min-width: 320px;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+
+  .post h3 {
+    font-size: 20px;
+    margin-top: 0;
+  }
+
+  .post .item-html {
+    line-height: 1.6;
+  }
+
+  .post img {
+    flex: 0 0 auto;
+    margin-left: 8;
+    border-radius: 4;
+    display: block;
+  }
+
+  @media (max-width: ${CONTAINER_WIDTH}px) {
+    .post {
+      flex-direction: column-reverse;
+    }
+    .post img {
+      margin: 8px auto;
+    }
+  }
+
+  .tags span {
+    background-color: #eee;
+    padding: 4px 8px;
+    border-radius: 4px;
+  }
+
+  .tags span + span {
+    margin-left: 8px;
+  }
+
+  .icons {
+    font-size: 14px;
+  }
+
+  .icons > span {
+    margin-right: 12px;
+  }
+
+  .icons .material-icons {
+    vertical-align: bottom;
+    display: inline-block;
+    margin-right: 4px;
+  }
+`
 
 function Posts () {
   useEffect(() => {
@@ -180,7 +276,7 @@ function Posts () {
   const { section, sectionsInfo } = useRouteData()
   const groupedPosts = groupPosts(section.items)
   return (
-    <div className="page">
+    <PostStyles className="page">
       <PostsHeader sections={sectionsInfo} currentSection={section} />
       <div className={section.items.length ? 'posts container card' : 'posts container'}>
         {groupedPosts.map(g => (
@@ -209,99 +305,7 @@ function Posts () {
         <FixedMenu groupedPosts={groupedPosts} />
       </div>
       <Footer></Footer>
-      <style jsx>{`
-        .page {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          background-color: #f8f8f8;
-        }
-
-        .container {
-          max-width: ${CONTAINER_WIDTH}px;
-          padding: 0 16px;
-          margin: 0 auto;
-        }
-
-        @media (min-width: ${CONTAINER_WIDTH}px) {
-          .card {
-            box-shadow: 2px 2px 6px 0px rgba(0,0,0,0.2);
-            border-radius: 8px;
-            background-color: white;
-          }
-
-          .posts {
-            margin-top: -24px;
-          }
-        }
-
-        .posts {
-          flex-grow: 1;
-          position: relative;
-        }
-
-        .posts h2 {
-          font-size: 30px;
-        }
-
-        .post {
-          min-width: 320px;
-          margin-bottom: 32px;
-          display: flex;
-          align-items: flex-start;
-          justify-content: space-between;
-        }
-
-        .post h3 {
-          font-size: 20px;
-          margin-top: 0;
-        }
-
-        .post .item-html {
-          line-height: 1.6;
-        }
-
-        .post img {
-          flex: 0 0 auto;
-          margin-left: 8;
-          border-radius: 4;
-          display: block;
-        }
-
-        @media (max-width: ${CONTAINER_WIDTH}px) {
-          .post {
-            flex-direction: column-reverse;
-          }
-          .post img {
-            margin: 8px auto;
-          }
-        }
-
-        .tags span {
-          background-color: #eee;
-          padding: 4px 8px;
-          border-radius: 4px;
-        }
-
-        .tags span + span {
-          margin-left: 8px;
-        }
-
-        .icons {
-          font-size: 14px;
-        }
-
-        .icons > span {
-          margin-right: 12px;
-        }
-
-        .icons .material-icons {
-          vertical-align: bottom;
-          display: inline-block;
-          margin-right: 4px;
-        }
-      `}</style>
-    </div>
+    </PostStyles>
   )
 }
 
