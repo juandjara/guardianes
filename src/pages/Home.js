28d686312a7839c348752fb31370c67da7ee5011
Juan D. Jara <juanorigami@gmail.com>
2020-06-07 01:44:33 +0200

    home styles to styled-components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,134 @@ import React from 'react'
 import SectionIcons from '../components/SectionIcons'
 import Social from '../components/Social'
 import { useRouteData, useSiteData } from 'react-static'
+import styled from 'styled-components'
+
+const HomeStyles = styled.div`
+  main {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+  }
+
+  .display-font {
+    font-family: Bree Serif, serif;
+  }
+
+  nav {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+
+  nav .site-logo {
+    padding: 1rem;
+    height: 92px;
+  }
+
+  nav header {
+    margin-top: 6px;
+    flex-grow: 1;
+  }
+
+  nav header .title {
+    line-height: 1.15;
+    font-size: 42px;
+    margin: 0;
+  }
+
+  nav header .subtitle {
+    font-size: 20px;
+    margin: 0;
+  }
+
+  nav .social {
+    margin-top: 8px;
+    margin-right: 16px;
+  }
+
+  .section-icons.right {
+    max-width: min(100vw, 628px);
+  }
+
+  .description {
+    max-width: 628px;
+    margin: 0 24px;
+    font-size: 18px;
+    line-height: 1.66;
+  }
+
+  .cta {
+    margin: 16px 24px;
+  }
+
+  .cta-first {
+    margin-right: 16px;
+  }
+
+  .cta-separator {
+    margin: 0 16px;
+  }
+
+  .cta-btn {
+    font-size: 18px;
+    border-radius: 16px;
+    border: none;
+    padding: 12px 18px;
+    margin: 8px 0;
+    background-color: #f0f0f0;
+    color: #ad1926;
+    cursor: pointer;
+    transition: opacity 0.25s ease;
+  }
+
+  .cta-btn.primary {
+    font-size: 20px;
+    padding: 16px 24px;
+  }
+
+  .cta-btn.outline {
+    background-color: transparent;
+    color: white;
+    border: 2px solid white;
+    padding: 10px 18px;
+  }
+
+  .cta-btn:hover {
+    opacity: 0.9;
+  }
+
+  .carrousel {
+    z-index: -1;
+    position: fixed;
+    top: 0;
+    right: 0;
+    max-width: 55%;
+    min-width: 300px;
+    overflow: hidden;
+    height: 100vh;
+    clip-path: polygon(0px 0%, 100% 0%, 100% 100%, 140px 100%);
+  }
+
+  .carrousel img {
+    max-width: none;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media (max-width: 812px) {
+    nav {
+      display: block;
+    }
+
+    nav header {
+      margin: 0 16px;
+    }
+
+    .carrousel {
+      min-width: 280px;
+    }
+  }
+`
 
 const Home = () => {
   const homeData = useSiteData()
@@ -9,8 +137,9 @@ const Home = () => {
   const firstImg = photos.data.filter(img => (
     img.media_type === 'IMAGE'
   ))[4].media_url
+
   return (
-    <div className="home">
+    <HomeStyles className="home">
       <main>
         <nav>
           <img className="site-logo" src="/images/escudo-flat-blanco.png" alt="logo" />
@@ -42,133 +171,7 @@ const Home = () => {
       <div className="carrousel">
         <img src={firstImg} />
       </div>
-      <style jsx>{`
-        main {
-          min-height: 100vh;
-          display: grid;
-          grid-template-rows: auto 1fr auto;
-        }
-
-        .display-font {
-          font-family: Bree Serif, serif;
-        }
-
-        nav {
-          display: flex;
-          align-items: flex-start;
-          justify-content: flex-start;
-        }
-
-        nav .site-logo {
-          padding: 1rem;
-          height: 92px;
-        }
-
-        nav header {
-          margin-top: 6px;
-          flex-grow: 1;
-        }
-  
-        nav header .title {
-          line-height: 1.15;
-          font-size: 42px;
-          margin: 0;
-        }
-  
-        nav header .subtitle {
-          font-size: 20px;
-          margin: 0;
-        }
-
-        :global(nav .social) {
-          margin-top: 8px;
-          margin-right: 16px;
-        }
-
-        :global(.section-icons.right) {
-          max-width: min(100vw, 628px);
-        }
-
-        .description {
-          max-width: 628px;
-          margin: 0 24px;
-          font-size: 18px;
-          line-height: 1.66;
-        }
-
-        .cta {
-          margin: 16px 24px;
-        }
-
-        .cta-first {
-          margin-right: 16px;
-        }
-
-        .cta-separator {
-          margin: 0 16px;
-        }
-  
-        .cta-btn {
-          font-size: 18px;
-          border-radius: 16px;
-          border: none;
-          padding: 12px 18px;
-          margin: 8px 0;
-          background-color: #f0f0f0;
-          color: #ad1926;
-          cursor: pointer;
-          transition: opacity 0.25s ease;
-        }
-  
-        .cta-btn.primary {
-          font-size: 20px;
-          padding: 16px 24px;
-        }
-  
-        .cta-btn.outline {
-          background-color: transparent;
-          color: white;
-          border: 2px solid white;
-          padding: 10px 18px;
-        }
-  
-        .cta-btn:hover {
-          opacity: 0.9;
-        }
-
-        .carrousel {
-          z-index: -1;
-          position: fixed;
-          top: 0;
-          right: 0;
-          max-width: 55%;
-          min-width: 300px;
-          overflow: hidden;
-          height: 100vh;
-          clip-path: polygon(0px 0%, 100% 0%, 100% 100%, 140px 100%);
-        }
-
-        .carrousel img {
-          max-width: none;
-          height: 100%;
-          object-fit: cover;
-        }
-
-        @media (max-width: 812px) {
-          nav {
-            display: block;
-          }
-
-          nav header {
-            margin: 0 16px;
-          }
-
-          .carrousel {
-            min-width: 280px;
-          }
-        }
-      `}</style>
-    </div>
+    </HomeStyles>
   )
 }
 
