f2994fbd9baeb93286c1af174a9f51e3ed8e0a80
Juan D. Jara <juanorigami@gmail.com>
2020-06-07 21:24:50 +0200

    add missing meta tag

--- static.config.js
+++ static.config.js
@@ -23,6 +23,7 @@ export default {
       <Head>
         <title>{siteData.titulo}</title>
         <meta name="description" content={siteData.subtitulo} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel='icon' type='image/png' href='/images/escudo-fullcolor.png'/>
         <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Roboto&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
